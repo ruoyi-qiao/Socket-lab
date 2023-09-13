ad07a2620add003f8969186f8f4f00edac46d7a8
Ruoyi Qiao <qry1317@163.com>
2023-09-13 21:13:12 +0800

    save

---
+++ Client.java
@@ -0,0 +1,165 @@
+package mySocket;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class Client {
+    private Socket socket;
+    private InputStream in;
+    private OutputStream out;
+    /**
+     * 构造函数 通过 IPaddress:port 构造socket
+     * @param address
+     * @param port
+     */
+    private Client(String address, String port) {
+        while (true) {
+            try {
+                this.socket = new Socket(address, Integer.parseInt(port));
+                this.in = socket.getInputStream();
+                this.out = socket.getOutputStream();
+                break;
+            } catch (IOException e) {
+                System.out.println("该(IP:端口)未在接听或已被占用,请修改");
+                Scanner sc = new Scanner(System.in);
+                System.out.println("请输入服务器IP：");
+                address = sc.next();
+                System.out.println("请输入端口号：");
+                port = sc.next();
+            } catch (IllegalArgumentException e) {
+                System.out.println("端口号不合法，请修改");
+                Scanner sc = new Scanner(System.in);
+                System.out.println("请输入服务器IP：");
+                address = sc.next();
+                System.out.println("请输入端口号：");
+                port = sc.next();
+            }
+        }
+        System.out.println("连接到服务器"+"("+address+","+port+")");
+    }
+    /**
+     * 启动客户端的 Reader线程 和 Writer 线程
+     */
+    public void start() {
+        Reader reader = new Reader();
+        Writer writer = new Writer();
+        reader.start();
+        writer.start();
+    }
+    /**
+     * 关闭连接并退出
+     */
+    public void close () {
+        try{
+            if(in != null) this.in.close();
+            if(out != null) this.out.close();
+            if(socket != null) this.socket.close();
+            System.exit(0);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private class Reader extends Thread {
+        private BufferedReader reader = new BufferedReader(new InputStreamReader (in, StandardCharsets.UTF_8));
+
+        @Override
+        public void run() {
+            try{
+                String line="";
+                while (!socket.isClosed() && line!=null) {
+                    line = reader.readLine();
+                    if(line != null){
+                        System.out.println(line);
+                    }
+                }
+                System.out.println("与服务器断开连接");
+                close();
+            } catch (IOException e) {
+                System.out.println("连接已断开");
+            } finally {
+                try {
+                    if (reader != null) reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    private class Writer extends Thread {
+        private PrintWriter writer = new PrintWriter(new OutputStreamWriter(out));
+        private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        @Override
+        public void run() {
+            try {
+                String line = "";
+                String message = "";
+
+                while(!socket.isClosed() && line != null) {
+                    boolean isFile = false;
+                    line = reader.readLine();
+
+                    for(int i = 0; i + 1 < line.length(); i++)
+                        if(line.charAt(i)=='-' && line.charAt(i+1)=='F')
+                            isFile = true;
+
+                    if(!isFile){
+                        if(!line.equals("")) {
+                            message += line + "\n";
+                        } else {
+                            for (int i = 0; i < message.length(); i++)
+                                writer.print(message.charAt(i));
+                            message = "";
+                            writer.flush();
+                        }
+                    }else {
+                        String[] path = line.split(" ");
+                        File file = new File(path[1]);
+                        BufferedReader br = new BufferedReader(new FileReader(file));
+                        while ((line = br.readLine()) != null) {
+                            System.out.println(line);
+                            writer.println(line);
+                            writer.flush();
+                        }
+                        br.close();
+                        line = "";
+                        System.out.println("文件信息发送完成.");
+                    }
+                }
+                System.out.println("客户端退出");
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("error:连接已关闭");
+            } finally {
+                try{
+                    if(writer!=null){
+                        writer.close();
+                    }
+                    if(reader!=null){
+                        reader.close();
+                    }
+                    close();
+                }catch(IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String address, port;
+        if(args.length == 2) {
+            address = args[0];
+            port = args[1];
+        } else {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("请输入服务器IP：");
+            address = sc.next();
+            System.out.println("请输入端口号：");
+            port = sc.next();
+        }
+        new Client(address, port).start();
+    }
+}
---
+++ Server.java
@@ -0,0 +1,200 @@
+package mySocket;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+
+public class Server {
+    private static long id = 0;
+    private HashMap<Long,ServerThread> socketList = new HashMap<>();
+    private ServerSocket serverSocket;
+    public Server(int port) {
+        try{
+            this.serverSocket = new ServerSocket(port);
+            System.out.println("服务器启动完成 使用端口："+port);
+            System.out.println("正在等待客户端的连接...");
+        } catch (IOException e) {
+            System.out.println("该段端口未处于listening或端口已被占用，请更换服务器端口!");
+            main(new String[0]);
+        }
+    }
+    /**
+     * 启动服务器，先让Writer对象启动等待键盘输入，然后不断等待客户端接入
+     * 如果有客户端接入，就可一个服务器线程放入Map中
+     */
+    public void start() {
+        new Writer().start();
+        try{
+           while(true) {
+               Socket socket = serverSocket.accept();
+               System.out.println(++id + "号客户端接入 (" + socket.getInetAddress()
+                       +","+ socket.getPort() + ")");
+               ServerThread thread = new ServerThread(id, socket);
+               socketList.put(id, thread);
+               thread.run();
+           }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * 关闭服务器
+     */
+    public void close() {
+        broadcast("服务器关闭中...");
+        try {
+            if(serverSocket!=null) {
+                serverSocket.close();
+            }
+        }catch (IOException e) {
+            System.out.println("服务器关闭中...");
+        }finally {
+            System.exit(0);
+        }
+    }
+    public void broadcast(String message, Long...args) {
+        LinkedList<Long> list = new LinkedList<>();
+        Set<Map.Entry<Long, ServerThread>> set = socketList.entrySet();
+        for(Map.Entry<Long, ServerThread> entry:set) {
+            if(args.length!=0 && Objects.equals(entry.getKey(), args[0])) continue;
+            list.add(entry.getKey());
+        }
+        for(Long id : list){
+            unicast(id, message);
+        }
+    }
+    public void unicast(Long id, String data) {
+        ServerThread socket = socketList.get(id);
+        if(socket!=null) socket.send(data);
+    }
+    private class ServerThread implements Runnable {
+        private Long id;
+        private Socket socket;
+        private InputStream in;
+        private OutputStream out;
+        private PrintWriter writer;
+
+        ServerThread(Long id, Socket socket) {
+            try{
+                this.id = id;
+                this.socket = socket;
+                this.in = socket.getInputStream();
+                this.out = socket.getOutputStream();
+                this.writer = new PrintWriter(new OutputStreamWriter(out));
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        @Override
+        public void run() {
+            new Reader().start();
+        }
+        public void send(String data) {
+            if(!socket.isClosed() && data!=null) {
+                writer.println(data);
+                writer.flush();
+            }
+        }
+        public void close(){
+            try{
+                if(writer!=null){
+                    writer.close();
+                }
+                if(in!=null){
+                    in.close();
+                }
+                if(out!=null){
+                    out.close();
+                }
+                if(socket!=null){
+                    socket.close();
+                }
+                socketList.remove(id);
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+        private class Reader extends Thread{
+            private BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            @Override
+            public void run() {
+                try{
+                    String line = "";
+                    while(!socket.isClosed() && line!=null && !"exit".equals(line)){
+                        line=reader.readLine();
+                        if(line!=null && !line.equals("exit")) {
+                            try {
+                                System.out.println("客户端"+id+"号: "+line);
+                            } catch (NumberFormatException e) {
+                                System.out.println("必须输入连接id号");
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                System.out.println("发送的消息不能为空");
+                            } catch (NullPointerException e) {
+                                System.out.println("连接不存在或已经断开");
+                            }
+                        }
+                    }
+                    System.out.println(id+":客户端主动断开连接");
+                    close();
+                }catch(IOException e) {
+                    System.out.println(id+":连接已断开");
+                }finally{
+                    try{
+                        if(reader!=null){
+                            reader.close();
+                        }
+                        close();
+                    }catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+    private class Writer extends Thread{
+        private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        @Override
+        public void run() {
+           String line="";
+           while(true) {
+               try{
+                   line = reader.readLine();
+                   if("exit".equals(line)) break;
+               }catch (IOException e) {
+                   e.printStackTrace();
+               }
+               if(line!=null && !line.equals("exit")){
+                   try{
+                       String[] data = line.split(":");
+                       if("*".equals(data[0])){
+                           broadcast("Server:"+data[1]);
+                       }else{
+                           unicast(Long.parseLong(data[0]),"Server:"+data[1]);
+                       }
+                       // 有可能发生的异常
+                   }catch(NumberFormatException e){
+                       System.out.print("必须输入连接id号\n");
+                   }catch(ArrayIndexOutOfBoundsException e){
+                       System.out.print("发送的消息不能为空\n");
+                   }catch(NullPointerException e){
+                       System.out.print("连接不存在或已经断开\n");
+                   }
+               }
+           }
+           System.out.println("服务器退出");
+           close();
+        }
+    }
+    public static void main(String[] args) {
+        int port;
+        if(args.length == 1) {
+            port = Integer.parseInt(args[0]);
+        } else {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("请输入端口号：");
+            port = sc.nextInt();
+        }
+        new Server(port).start();
+    }
+}
